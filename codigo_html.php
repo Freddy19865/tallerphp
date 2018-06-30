010924354b9dbe337391dd4aa3434c049b68877f
Freddyuzp <Freddy.uzp@gmail.com>
2018-06-30 06:46:46 -0430

    segunda parte del taller

---
+++ codigo_html.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Insertar PHP en código HTML </title>
+    <meta charset="utf-8">
+
+    <?php 
+
+    	$norte= 5;
+    	$sur= 6;
+    	$centro=4;
+    	$total_regiones=$norte+$sur+$centro;
+    	$texto="chile esta dividido en: ";
+
+    	echo "$texto $total_regiones Regiones </br> 
+    		</br>";
+
+    	echo "$norte Norte <br>
+    		  $sur	Sur <br>
+    		  $centro Centro<br>
+    		";
+
+
+     ?>
+
+
+</head>
+<body>
+    
+    
+    
+</body>
+</html>
---
+++ primeros_pasos.php
@@ -0,0 +1,5 @@
+<?php  
+
+echo "primera pagina con php <br>";
+echo "segunda linea";
+?>
